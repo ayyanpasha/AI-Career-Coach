ff7048ffdf52996dd3893a597292fa10fad06e66
ayyanpasha <ayyanpasha2014@gmail.com>
2025-03-29 16:46:55 +0530

    First Page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import Header from "@/components/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,24 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {/* header */}
+            <Header />
+            <main className="min-h-screen">{children}</main>
+            {/* footer */}
+            <footer>
+              <div className="bg-muted/50 py-12">
+                <p className="container mx-auto px-4 text-center text-gray-200">
+                  Made with Love with Ayyan Pasha
+                </p>
+              </div>
+            </footer>
+          </ThemeProvider>
       </body>
     </html>
   );
